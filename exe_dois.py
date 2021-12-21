d0a06120692571d38cf02fc62891de70f76c7a30
Alyne Thaisa Leonardo <65407401+alynethaisa@users.noreply.github.com>
2021-12-21 20:42:15 -0300

    Exercícios
    FIAP - Python

---
+++ exe_dois.py
@@ -0,0 +1,6 @@
+nome=input("Digite o nome: ")
+idade=int(input("Qual a sua idade?"))
+
+
+print(nome)
+print(idade)
---
+++ exe_quatro.py
@@ -0,0 +1,5 @@
+numero=int(input("Digite um número: "))
+while numero<50:
+  print ("\t" + str(numero))
+  numero=numero+1
+print ("Laço encerrado..")
